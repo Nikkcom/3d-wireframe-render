d20c63bd6d432ef9cd4e9181de78dca5fd919dd8
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2026-03-25 22:42:56 +0100

    feat: add command line args

--- src/main/java/me/niko/Main.java
+++ src/main/java/me/niko/Main.java
@@ -1,6 +1,8 @@
 package me.niko;
 
 import me.niko.models.*;
+import me.niko.models.shapes.CubeModel;
+import me.niko.models.shapes.DiamondModel;
 import me.niko.view.ModelView;
 
 import javax.swing.*;
@@ -8,14 +10,48 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 public class Main {
+
     public static void main(String[] args) {
-
         final Color BACKGROUND_COLOR = Color.DARK_GRAY;
-        final Color MODEL_COLOR = Color.RED;
         final double MODEL_SIZE = 1.0;
-        final ViewableModel MODEL = new CubeModel(MODEL_SIZE);
+        Model modelShape = Model.CUBE;
+        Color modelColor = Color.RED;
+        String shapeStr = "";
+        String colorStr = "";
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-s":
+                case "--shape":
+                    if (i + 1 < args.length) {
+                        shapeStr = args[++i];
+                    }
+                    break;
+                case "-c":
+                case "--color":
+                    if (i + 1 < args.length) {
+                        colorStr = args[++i];
+                    }
+                    break;
+            }
+        }
+        if (!shapeStr.isEmpty()) {
+            try {
+                modelShape = Model.valueOf(shapeStr.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Could not parse shape: " + shapeStr);
+            }
+        }
 
-        ModelView view = new ModelView(MODEL_COLOR, MODEL, new Dimension(500, 500));
+        if (!colorStr.isEmpty()) {
+            try {
+                modelColor = Color.decode(colorStr.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Could not parse color: " + colorStr);
+            }
+        }
+        ViewableModel model = createModel(modelShape, MODEL_SIZE);
+
+        ModelView view = new ModelView(modelColor, model, new Dimension(500, 500));
         JFrame frame = new JFrame();
         frame.setTitle("3D visning");
         frame.setContentPane(view);
@@ -33,12 +69,19 @@ public class Main {
             double deltaAngleXY = rotationSpeed * dt;
             double deltaAngleYZ = rotationSpeed * dt * 2;
             double deltaAngleXZ = rotationSpeed * dt * 3;
-            for (Point3D p : MODEL.getVertices()) {
+            for (Point3D p : model.getVertices()) {
                 p.rotateXZ(deltaAngleXZ);
                 p.rotateYZ(deltaAngleYZ);
                 p.rotateXY(deltaAngleXY);
             }
             view.repaint();
         }).start();
+    }
+
+    private static ViewableModel createModel(Model model, double size) {
+        return switch (model) {
+            case CUBE -> new CubeModel(size);
+            case DIAMOND -> new DiamondModel(size);
+        };
     }
 }
